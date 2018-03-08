aaf9ddf4c6c0098c2913f590916ae01d1df0ab51
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-08 16:26:31 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,6 +1,6 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[5.1]
-  def change 
+  def change
     create_table :costume_stores
       t.string :name
       t.string :location
@@ -8,14 +8,9 @@ class CreateCostumeStores < ActiveRecord::Migration[5.1]
       t.integer :number_of_employees
       t.integer :still_in_business
       t.datetime :opening_time
-      t.datetime :closing_time 
+      t.datetime :closing_time
+    end 
+  end 
+end 
 
 
-
-    name
-location
-number of costumes, or "costume inventory"
-number of employees
-whether or not it's still in business
-opening time
-closing time
