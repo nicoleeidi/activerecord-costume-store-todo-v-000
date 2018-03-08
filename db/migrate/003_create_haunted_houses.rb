464c434f863180b4bdfea18accd11500968d8f96
Nicole Eidi <nicoleeidi1@gmail.com>
2018-03-08 16:44:24 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -10,9 +10,9 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
-    end 
-  end 
-end 
+    end
+  end
+end
 
 
 
